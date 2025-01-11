c474170bd5ff8359059342a5c8b0ad7258b831e2
Anurag choudhary <anurag.jay1002@gmail.com>
2025-01-11 15:14:13 +0530

    ducks and Quack added (#1)

---
+++ QuackBehaviour/QuackWithSound.java
@@ -0,0 +1,10 @@
+package HeadFirstDesignPattern.QuackBehaviour;
+
+public class QuackWithSound implements QuackBehaviour {
+
+    @Override
+    public void quack() {
+        System.out.println("Quack Quack");
+    }
+    
+}
