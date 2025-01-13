a30bf555ac58d85d2b04c8b98fb0f7a7eca98061
ycwhencpp <anurag.jay1002@gmail.com>
2025-01-14 00:37:36 +0530

    Condiments added

---
+++ DecoratorPattern/StarBuzz/Condiments/Condiments.java
@@ -0,0 +1,9 @@
+package HeadFirstDesignPattern.DecoratorPattern.StarBuzz.Condiments;
+
+import HeadFirstDesignPattern.DecoratorPattern.StarBuzz.Beverages.Beverage;
+
+public abstract class Condiments extends Beverage {
+
+    public abstract String getDescription();
+
+}
