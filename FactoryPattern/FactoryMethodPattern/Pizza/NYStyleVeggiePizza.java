303aa24206673fa8a2636d429b7053715aeae444
ycwhencpp <anurag.jay1002@gmail.com>
2025-01-16 01:18:22 +0530

    singelton pattern added

---
+++ FactoryPattern/FactoryMethodPattern/Pizza/NYStyleVeggiePizza.java
@@ -0,0 +1,12 @@
+package HeadFirstDesignPattern.FactoryPattern.FactoryMethodPattern.Pizza;
+
+import java.util.ArrayList;
+
+public class NYStyleVeggiePizza extends Pizza {
+    public NYStyleVeggiePizza(String name, String Dough, String sauce, ArrayList toppings){
+        this.name = name;
+        this.Dough = Dough;
+        this.sauce = sauce;
+        this.toppings = toppings;
+    }
+}
